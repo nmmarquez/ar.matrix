9f5a98c167600c6bc633f15f9cee210b85fdca00
nmmarquez <nmmarquez@gmail.com>
2017-01-20 00:50:25 -0600

    add documentation for sim ar1 showing kronecker product

--- R/simAR.R
+++ R/simAR.R
@@ -12,6 +12,29 @@
 #' @return Matrix object, matrix where each row is a single obsrevation from
 #' a GMRF with covariance structure Q^-1.
 #'
+#' @examples
+#' require("ggplot2")
+#'
+#' # simulate 2D ar1 process
+#' # pairwise correlation
+#' rho <- .95
+#' # pairwise variance
+#' sigma <- .5
+#'
+#' # 2 dimensions of simulations
+#' years <- 20
+#' ages <- 10
+#'
+#' # kronnecker product to get joint covariance
+#' Q2D <- kronecker(Q.AR1(M=years, sigma, rho), Q.AR1(M=ages, sigma, rho))
+#'
+#' # simulate the data and place it in a data frame
+#' Q2D.df <- data.frame(obs=c(sim.AR(1, Q2D)), age=rep(1:ages, years),
+#'                      year=rep(1:years, each=ages))
+#'
+#' # graph results
+#' ggplot(data=Q2D.df, aes(year, obs, group=age, color=age)) + geom_line()
+#'
 #' @export
 
 sim.AR <- function(n, Q){
